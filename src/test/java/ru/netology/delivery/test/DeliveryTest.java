aac827caa8acf56cfc400a1000f0b60b39cf1130
ArtVysh <vysh-artem@yandex.ru>
2024-01-08 16:32:26 +0400

    change test at line 53

--- src/test/java/ru/netology/delivery/test/DeliveryTest.java
+++ src/test/java/ru/netology/delivery/test/DeliveryTest.java
@@ -50,9 +50,8 @@ class DeliveryTest {
         $("[data-test-id=date] .input__control").setValue(secondMeetingDate);
         $(byText("Запланировать")).click();
         $("[data-test-id='replan-notification'] .notification__content")
-                .shouldHave(exactText("У вас уже запланирована встреча на другую дату. Перепланировать?"))
-                /*.shouldBe(visible);*/
-                .shouldBe(visible, Duration.ofSeconds(1));
+                .shouldHave(ownText("У вас уже запланирована встреча на другую дату. Перепланировать?"))
+                .shouldBe(visible);
         $("[data-test-id='replan-notification'] button").click();
         $("[data-test-id=success-notification] .notification__content")
                 .shouldHave(exactText("Встреча успешно запланирована на " + secondMeetingDate))
